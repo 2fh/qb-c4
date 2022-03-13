b5f1bf060086018b61f5931ed789cec7d2de20fd
2fh <40892352+2fh@users.noreply.github.com>
2022-03-14 01:49:48 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Ra.#9333'
+
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_script 'config.lua'
