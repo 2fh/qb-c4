8e80a60fd7ee2fcc40fde69de7cc4ce0c8c3d8f3
2fh <40892352+2fh@users.noreply.github.com>
2022-03-14 01:52:55 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+ 
+Config.UsingMythicNotifications = true -- Dont Touch
+
+Config.UsingProgressBars = true -- Dont Touch
+
+Config.TimeTakenToArm = 3 -- Timer
+
+Config.TimeUntilDetonation = 10  -- Timer For C4 To Detonation/Bomb
